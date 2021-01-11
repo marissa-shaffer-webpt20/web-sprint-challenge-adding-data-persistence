5850e256aaef5b4f05365523ffc754ab405bc9e2
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-10 21:59:34 -0600

    Set up index file and dbconfig

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express")
+const helmet = require("helmet")
+const projectRouter = require("./api/project")
+const resourceRouter = require("./api/resource")
+const taskRouter = require("./api/task")
 
 const server = express()
 const port = process.env.PORT || 8000
 
+server.use(helmet())
 server.use(express.json())
 
-server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`)
+server.use(projectRouter)
+server.use(resourceRouter)
+server.use(taskRouter)
+
+server.get("/", (req, res) =>{
+    res.send("Sprint DB API for Lambda")
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
 })
