97a376388b3aebd53c815fff0fd0bd53888fe5d4
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-10 23:57:03 -0600

    completed endpoints

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,30 @@
 // build your `/api/projects` router here
+const express = require("express")
+const project = require("./model")
+
+const router = express.Router()
+
+//GET PROJECTS
+router.get("/projects", async (req, res, next) => {
+    try {
+        const projects = await project.getProjects()
+        res.json(projects)
+    } catch(err) {
+        next(err)
+    }
+})
+
+//CREATE PROJECT
+router.post('/projects', (req, res) => {
+    const projectData = req.body;
+
+    project.addProject(projectData)
+        .then(project => {
+        res.status(201).json(project);
+    })
+        .catch (err => {
+        res.status(500).json({ message: 'Failed to create new project' });
+    });
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,30 @@
 // build your `/api/tasks` router here
+const express = require("express")
+const task = require("./model")
+
+const router = express.Router()
+
+//GET TASKS
+router.get("/tasks", async (req, res, next) => {
+    try {
+        const tasks = await task.getTasks()
+        res.json(tasks)
+    } catch(err) {
+        next(err)
+    }
+})
+
+//CREATE TASK
+router.post('/task', (req, res) => {
+    const taskData = req.body;
+
+    task.addTask(taskData)
+        .then(task => {
+        res.status(201).json(task);
+    })
+        .catch (err => {
+        res.status(500).json({ message: 'Failed to create new task' });
+    });
+})
+
+module.exports = router
