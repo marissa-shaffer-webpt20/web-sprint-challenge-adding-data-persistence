85ede06d015bf4c433bfb59ad61ccd87a13a1382
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-10 23:43:13 -0600

    set up project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,34 @@
 // build your `Project` model here
+const db = require("../data/dbConfig")
+
+function getProjects() {
+    return db("projects")
+}
+
+function findById(id) {
+    return db("projects")
+        .where({id})
+        .first();
+}
+
+function addProject(project) {
+    return db("projects")
+        .insert(project)
+        .then(id => {
+            return findById(id[0]);
+        });
+}
+
+function resourcesByProject(id) {
+    return db("projects as p")
+        .where("p.id", id)
+        .join("resources as r", "r.project_id", "p.id")
+        .select("r.name", "r.description")
+}
+
+module.exports = {
+    getProjects,
+    findById,
+    addProject,
+    resourcesByProject,
+}
