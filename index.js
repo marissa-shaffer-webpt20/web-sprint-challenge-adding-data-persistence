3065395172fffadf6582aed6461146bdab9f91dd
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-10 21:22:14 -0600

    Set up server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// start your server here
+const express = require("express")
+
+const server = express()
+const port = process.env.PORT || 8000
+
+server.use(express.json())
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
