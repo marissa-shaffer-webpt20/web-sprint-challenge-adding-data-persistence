b4e7f30312f9960cf1f427cc83f4c8c5e34d015a
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-10 23:44:11 -0600

    Set up resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,26 @@
 // build your `Resource` model here
+const db = require("../data/config")
+
+function getResources() {
+    return db("resources")
+}
+
+function findById(id) {
+    return db("projects")
+        .where({id})
+        .first();
+}
+
+function addResource(resource) {
+    return db("resources")
+        .insert(resource)
+        .then(id => {
+            return findById(id[0]);
+        });
+}
+
+module.exports = {
+    findById,
+    getResources,
+    addResource,
+}
